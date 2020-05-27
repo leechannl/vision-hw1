06dff809ff37225de25c3ab4a0ed35411c329ffe
leechannl <leechannl@gmail.com>
2020-05-27 18:54:55 +0800

    add gaussian filter

--- src/filter_image.c
+++ src/filter_image.c
@@ -157,15 +157,37 @@ image make_hemboss_filter() {
 
 // Question 2.2.1: Which of these filters should we use preserve when we run our convolution and which ones should we
 // not? Why?
-// Answer: when high
-//
+// Answer:
+//    Kernel sum to 1, then the original color will be preseved, so `preserve` should be 1, otherwise be 0.
+//    So for highpass filter `preserve` be 0, others be 1.
 
 // Question 2.2.2: Do we have to do any post-processing for the above filters? Which ones and why?
-// Answer: TODO
+// Answer:
+//    Need to do clamp image. Because after doing convolution, the pixel value is likely greater than 1.
 
 image make_gaussian_filter(float sigma) {
-  // TODO
-  return make_image(1, 1, 1);
+  // g(x,y) = 1/(TWO_PI*pow(sigma,2)) * exp(-( (pow(x-mean,2)+pow(y-mean,2)) / (2*pow(sigma,2)) ))
+  // g(x,y) = multiplier * exp(exponent)
+  // multiplier = 1/(TWO_PI*pow(sigma,2))
+  // exponent = -( (pow(x-mean,2)+pow(y-mean,2)) / (2*pow(sigma,2)) )
+
+  int six_sigma = (int)ceilf(sigma * 6);
+  int size = six_sigma % 2 ? six_sigma : six_sigma + 1;
+
+  image f = make_image(size, size, 1);
+
+  float multiplier = 1 / (TWOPI * sigma * sigma);
+  int mean = size / 2;
+
+  for (int i = 0; i < size; i++) {
+    for (int j = 0; j < size; j++) {
+      float exponent = -(pow(i - mean, 2) + pow(j - mean, 2)) / (2 * sigma * sigma);
+      float v = multiplier * exp(exponent);
+      set_pixel(f, i, j, 0, v);
+    }
+  }
+  l1_normalize(f);
+  return f;
 }
 
 image add_image(image a, image b) {
